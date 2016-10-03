41ba3b1beca044cfb28b87f94e2264093b666424
K0a1a <github@im4youplay.de>
2016-10-03 21:06:18 +0200

    Add files via upload

---
+++ slave.ino
@@ -0,0 +1,28 @@
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  mySwitch.enableTransmit(10);
+
+  mySwitch.setProtocol(1);
+  
+}
+
+void loop() {
+
+  while(Serial.available()){
+    int ser = Serial.parseInt();
+    if(ser == 1){
+      mySwitch.send(16765268, 24);
+    }else if(ser == 0){
+      mySwitch.send(16765265, 24);
+    }
+  }
+
+  delay(100);
+
+}
